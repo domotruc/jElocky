21acde9eec5080f86ee62c747661d98d0fe89739
domotruc <domotruc@gmail.com>
2019-01-02 08:47:25 +0100

    Code cleaning

--- desktop/php/jElocky.php
+++ desktop/php/jElocky.php
@@ -17,6 +17,7 @@ $eqPlaces = eqLogic::byType('jElocky_place');
 
 /* @var array[jElocky_object] $eqObjects */
 /**
+ *
  * @var Ambiguous $eqObjects
  */
 $eqObjects = eqLogic::byType('jElocky_object');
@@ -24,22 +25,24 @@ $eqObjects = eqLogic::byType('jElocky_object');
 function displayActionCard($_action_name, $_action, $_eqLogic_type, $_fa_icon, $_card_color) {
     $eqLogic_type = isset($_eqLogic_type) ? ' data-eqLogic_type="' . $_eqLogic_type . '"' : '';
     echo '<div class="cursor eqLogicAction" data-action="' . $_action . '"' . $eqLogic_type .
-        ' style="text-align:center;background-color:#ffffff;height:200px;width:160px;margin-bottom:10px;padding:5px;border-radius:2px;margin-right:10px;float:left;" >';
+        ' style="text-align:center;height:200px;width:160px;margin-bottom:10px;padding:5px;border-radius:2px;margin-right:10px;float:left;" >';
     echo '<div class="center-block" style="width:130px;height:130px;display:flex;align-items: center;justify-content:center;">';
     echo '<i class="fa ' . $_fa_icon . '" style="font-size:6em;color:' . $_card_color . ';"></i>';
     echo "</div>";
-    echo '<span style="font-size:1.1em;font-weight:bold;position:relative;top:10px;word-break:break-all;white-space:pre-wrap;word-wrap:break-word;">' . $_action_name . '</span>';
+    echo '<span style="font-size:1.1em;font-weight:bold;position:relative;top:10px;word-break:break-all;white-space:pre-wrap;word-wrap:break-word;">' .
+        $_action_name . '</span>';
     echo '</div>';
 }
 
 /**
+ *
  * @param eqLogic $_eqLogic
  */
 function displayEqLogicCard($_eqLogic) {
     $opacity = $_eqLogic->getIsEnable() ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
     echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $_eqLogic->getId() . '" data-eqLogic_type="' .
         get_class($_eqLogic) .
-        '" style="text-align:center;background-color:#ffffff;height:200px;width:160px;margin-bottom:10px;padding:5px;border-radius:2px;margin-right:10px;float:left;' .
+        '" style="text-align:center;height:200px;width:160px;margin-bottom:10px;padding:5px;border-radius:2px;margin-right:10px;float:left;' .
         $opacity . '" >';
     echo '<div class="center-block" style="width:130px;height:130px;background-color:#f8d800;border-radius:30px;display:flex;align-items:center;">';
     echo '<img src="' . $_eqLogic->getPhoto() .
@@ -56,7 +59,8 @@ function displayEqLogicCard($_eqLogic) {
     <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <a class="btn btn-default eqLogicAction" style="width: 100%; margin-top: 5px; margin-bottom: 5px;"
-                data-action="add" data-eqLogic_type="jElocky_user"><i class="fa fa-plus-circle"></i> {{Ajouter un utilisateur}}</a>
+                data-action="add" data-eqLogic_type="jElocky_user"><i class="fa fa-plus-circle"></i> {{Ajouter un
+                utilisateur}}</a>
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
                 <li><i class="fa fa-male"></i><span> {{Utilisateurs}}</span>
                     <ul class="nav nav-list bs-sidenav sub-nav-list">
@@ -99,29 +103,29 @@ function displayEqLogicCard($_eqLogic) {
         style="border-left: solid 1px #EEE; padding-left: 25px;">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-6">
-                <legend>
-                    <i class="fa fa-male"></i> {{Utilisateurs}}
-                </legend>
-                <?php
-                foreach ($eqUsers as $eqU) {
-                    displayEqLogicCard($eqU);
-                }
-                displayActionCard('{{Ajouter un utilisateur}}', 'add', 'jElocky_user', 'fa-plus-circle', '#f8d800');
-                ?>
+                <form><fieldset>
+                    <legend><i class="fa fa-male"></i> {{Utilisateurs}}</legend>
+                    <?php
+                    foreach ($eqUsers as $eqU) {
+                        displayEqLogicCard($eqU);
+                    }
+                    displayActionCard('{{Ajouter un utilisateur}}', 'add', 'jElocky_user', 'fa-plus-circle', '#f8d800');
+                    ?>
+                </fieldset></form>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6">
-                <legend>
-                    <i class="fa fa-cog"></i> {{Gestion}}
-                </legend>
+                <form><fieldset>
+                    <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
                     <?php
                     displayActionCard('{{Configuration}}', 'gotoPluginConf', null, 'fa-wrench', '#767676');
                     ?>
+                </fieldset></form>
             </div>
         </div>
 
         <?php
         if (count($eqPlaces) > 0) {
-            echo '<legend><i class="fa fa-home"></i> {{Lieux}}</legend>';
+            echo '<form><fieldset><legend><i class="fa fa-home"></i> {{Lieux}}</legend>';
             echo '<ul class="nav nav-tabs" role="tablist">';
             $active = ' class="active"';
             foreach ($eqPlaces as $eqP) {
@@ -142,18 +146,16 @@ function displayEqLogicCard($_eqLogic) {
                 $active = '';
                 $eqObjects = $eqP->getObjects();
                 if (count($eqObjects) > 0) {
-                    echo '<legend><i class="fa fa-key"></i>  {{Objets}}</legend>';
-                    // echo '<div class="eqLogicThumbnailContainer">';
-                }
-                foreach ($eqObjects as $eqO) {
-                    displayEqLogicCard($eqO);
-                }
-                if (count($eqObjects) > 0) {
-                    // echo '</div>';
+                    echo '<form><fieldset><legend><i class="fa fa-key"></i>  {{Objets}}</legend>';
+                    foreach ($eqObjects as $eqO) {
+                        displayEqLogicCard($eqO);
+                    }
+                    echo '</fieldset></form>';
                 }
                 echo '</div>';
             }
             echo '</div>';
+            echo '</fieldset></form>';
         }
         ?>
     </div>
